f1a0616b33bae361e18b30bfb4f19f47de32d440
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-25 17:13:59 +0100

    debug

--- src/listeners/debug.ts
+++ src/listeners/debug.ts
@@ -4,8 +4,8 @@ import * as userServices from "../services/user";
 import * as subscriptionServices from "../services/subscription";
 
 export default async (props: any, event: any, api: any) => {
-    console.log(await userServices.create(api, { name: "Jonas", identifier: "jonas.martinez" }));
+    console.log((await userServices.create(api, { name: "Jonas", identifier: "jonas.martinez" })).data);
     console.log(await userServices.getByIdentifier(api, "jonas.martinez"));
-    console.log(await subscriptionServices.subscribeTo(api, "jonas.martinez"));
+    console.log((await subscriptionServices.subscribeTo(api, "jonas.martinez")).data);
     return {};
 }
