78fcf90f4e5b8af43094f6fe142c5bf5a674b492
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-25 10:45:26 +0100

    fix

--- services/subscription.ts
+++ services/subscription.ts
@@ -4,8 +4,8 @@ import * as UserServices from './user';
 import * as Api from './api';
 
 type Subscription = {
-    subscriber_id: string;
-    target_user_id: string;
+    subscriber_id: String;
+    target_user_id: String;
 };
 
 async function create(api, subscriptionData: Subscription) {
@@ -13,12 +13,12 @@ async function create(api, subscriptionData: Subscription) {
 }
 
 async function subscribeTo(api, targetUserId: String) {
-    let user = await UserServices.get(api);
-    return Api.createDoc(api, "subscriptions", { subscriber_id: user._id, target_user_id: targetUserId });
+    const user = await UserServices.get(api);
+    return create(api, { subscriber_id: user._id, target_user_id: targetUserId });
 }
 
 async function unsubscribeFrom(api, targetUserId: String) {
-    const user = await this.get(api);
+    const user = await UserServices.get(api);
     const subscription = await Api.executeQuery(api, "subscriptions", {
         "subscriber_id": "@me",
         "target_user_id": targetUserId,
