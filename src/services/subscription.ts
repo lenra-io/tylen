ad7e3b8d2602059414c39ef54bacbf55c99c5d99
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-25 11:44:06 +0100

    fix

--- src/services/subscription.ts
+++ src/services/subscription.ts
@@ -8,24 +8,25 @@ type Subscription = {
     target_user_id: String;
 };
 
-module.exports = {
-    async create(api, subscriptionData: Subscription) {
-        return Api.createDoc(api, "subscriptions", subscriptionData);
-    },
 
-    async subscribeTo(api, targetUserIdentifier: String) {
-        const user = await UserServices.get(api);
-        const targetUser = await UserServices.getByIdentifier(api, targetUserIdentifier);
-        return this.create(api, { subscriber_id: user._id, target_user_id: targetUser._id });
-    },
-
-    async unsubscribeFrom(api, targetUserIdentifier: String) {
-        const user = await UserServices.get(api);
-        const targetUser = await UserServices.getByIdentifier(api, targetUserIdentifier);
-        const subscription = await Api.executeQuery(api, "subscriptions", {
-            "subscriber_id": user._id,
-            "target_user_id": targetUser._id,
-        });
-        return await Api.deleteDoc(api, "subscriptions", subscription.data[0]);
-    }
+async function create(api, subscriptionData: Subscription) {
+    return Api.createDoc(api, "subscriptions", subscriptionData);
 }
+
+async function subscribeTo(api, targetUserIdentifier: String) {
+    const user = await UserServices.get(api);
+    const targetUser = await UserServices.getByIdentifier(api, targetUserIdentifier);
+    return this.create(api, { subscriber_id: user._id, target_user_id: targetUser._id });
+}
+
+async function unsubscribeFrom(api, targetUserIdentifier: String) {
+    const user = await UserServices.get(api);
+    const targetUser = await UserServices.getByIdentifier(api, targetUserIdentifier);
+    const subscription = await Api.executeQuery(api, "subscriptions", {
+        "subscriber_id": user._id,
+        "target_user_id": targetUser._id,
+    });
+    return await Api.deleteDoc(api, "subscriptions", subscription.data[0]);
+}
+
+export { Subscription, create, subscribeTo, unsubscribeFrom };
