ce0eaaef606093ad4a1c71d35ac157926b7d22ef
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-29 15:45:01 +0100

    Remove some text

--- src/widgets/firstTimePage.ts
+++ src/widgets/firstTimePage.ts
@@ -10,14 +10,14 @@ export default async (data: any, props: any) => {
                 type: "text",
                 value: "Welcome on Tylen!"
             },
-            {
-                type: "text",
-                value: "Please enter your username it is the name that will be shown to other users."
-            },
-            {
-                type: "text",
-                value: "And your identifier which can be used by other users to find you on Tylen."
-            },
+            // {
+            //     type: "text",
+            //     value: "Please enter your username it is the name that will be shown to other users."
+            // },
+            // {
+            //     type: "text",
+            //     value: "And your identifier which can be used by other users to find you on Tylen."
+            // },
         ]
     }
 }
