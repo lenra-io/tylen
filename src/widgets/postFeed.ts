579c6fe111581c74a2b5fe30d6c4dd3bb4b2689a
jonas-martinez <cbobbxta.fr@gmail.com>
2022-12-09 11:56:38 +0100

    add todo comment

--- src/widgets/postFeed.ts
+++ src/widgets/postFeed.ts
@@ -1,6 +1,7 @@
 'use strict'
 
 export default (data: any, props: any) => {
+    // TODO get posts from database and show them from newest to oldest
     return {
         type: "text",
         value: "TODO Post Feed"
