8cedece3f0791c025cf0ff04a9b52edff099dfd2
jonas-martinez <cbobbxta.fr@gmail.com>
2022-12-15 15:46:22 +0100

    Perf problem

--- src/listeners/onEnvStart.ts
+++ src/listeners/onEnvStart.ts
@@ -1,5 +1,13 @@
 'use strict'
 
+import * as apiServices from "../services/api";
+
 export default async (props: any, event: any, api: any) => {
+    await apiServices.createDoc(api, "users", { id: "debug", name: "debug", identifier: "debug", errors: {} });
+
+    for (let i = 0; i < 150; i++) {
+        await apiServices.createDoc(api, "posts", { creatorId: "debug", text: "This is a debug message" });
+    }
+
     return {}
 }
