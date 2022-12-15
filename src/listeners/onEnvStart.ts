294d6a75133d447849f04661efe0394c25dd58d6
jonas-martinez <cbobbxta.fr@gmail.com>
2022-12-15 15:48:10 +0100

    debug

--- src/listeners/onEnvStart.ts
+++ src/listeners/onEnvStart.ts
@@ -6,7 +6,7 @@ export default async (props: any, event: any, api: any) => {
     await apiServices.createDoc(api, "users", { id: "debug", name: "debug", identifier: "debug", errors: {} });
 
     for (let i = 0; i < 150; i++) {
-        await apiServices.createDoc(api, "posts", { creatorId: "debug", text: "This is a debug message" });
+        await apiServices.createDoc(api, "posts", { creatorId: "debug", text: `This is a debug message ${i}` });
     }
 
     return {}
